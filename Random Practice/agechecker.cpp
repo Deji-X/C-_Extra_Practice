a45244492fb7e12cfd30a60719bedb3b14a5b462
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-20 17:45:02 +0100

    Create agechecker.cpp

---
+++ Random Practice/agechecker.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write your code below
+    int age;
+    cout << "how old are you?" <<endl;
+    cin >> age;
+
+    int years = 120 - age;
+
+    cout << years << " years till 120" << endl;
+
+
+    return 0;
+}

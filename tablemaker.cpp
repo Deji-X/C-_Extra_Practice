826122f5f26de36201fe127780ece40610845cc0
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-20 13:20:04 +0100

    Create tablemaker.cpp

---
+++ tablemaker.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write code here
+    cout << "C++ is fun!" << endl;
+    cout << "Let's explore its features:" << endl;
+    cout << "1. Variables\t2. Loops" <<endl;
+    cout << "3. Functions\t4. Classes" <<endl;
+    cout << "Happy coding!" <<endl;
+    return 0;
+};

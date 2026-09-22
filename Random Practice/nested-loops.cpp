577a1e757f6cea042d9a54b10feea3776fc7079b
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-22 15:39:31 +0100

    Create nested-loops.cpp

---
+++ Random Practice/nested-loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 1; i < n; i++){
+        for ( int j = 1; j < n; j++) {
+            if (i + j == n){
+            std::cout << i << " " << j << std::endl;}
+        }
+    }
+
+    return 0;
+}

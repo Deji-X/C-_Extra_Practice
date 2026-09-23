934121a240f849152746de9ecf1cae031c9c2e2c
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-23 17:28:22 +0100

    Add comment on function overloading
    Add a comment explaining function overloading.

---
+++ Random Practice/Function_overloading.cpp
@@ -0,0 +1,4 @@
+/*
+Function Overloading
+Function overloading allows multiple functions with the same name but different parameters.
+The compiler selects the appropriate function based on argument types:*/

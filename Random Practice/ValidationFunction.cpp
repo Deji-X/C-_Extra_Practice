39b3e9736b66a05ad63e3fa95f0feccde16df086
Henry Wisdom <henry6769wisdom@gmail.com>
2026-09-25 09:28:26 -0700

    Create ValidationFunction.cpp

---
+++ Random Practice/ValidationFunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+/*
+Write a function named is_valid that gets two string arguments, username and password.
+
+The function will return true if the username and
+password are valid in the system, otherwise false.
+
+Our system contains only two valid usernames - "admin" and "user".
+
+The valid password for username "user" is "qwerty".
+
+For username "admin" any password is valid!
+*/
+int main(
+
+
+
+return 0;
+);
